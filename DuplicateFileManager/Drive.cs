2ceb1a671b746ee85eaced59344c2dff8f90dc38
Kelly Anderson <kellycoinguy@gmail.com>
2013-11-13 16:24:34 -0700

    Initial Commit

---
+++ DuplicateFileManager/Drive.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DuplicateFileManager
+{
+    public class Drive
+    {
+        private char _driveLetter;
+        public Drive(char driveLetter)
+        {
+            _driveLetter = driveLetter;
+        }
+
+        public char DriveLetter { get { return _driveLetter; } }
+    }
+}
---
+++ DuplicateFileManager/Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DuplicateFileManager
+{
+    [TestFixture]
+    class Tests
+    {
+        [Test]
+        public void TestFrameworkIsWorking()
+        {
+            Assert.That(true, Is.True);
+        }
+
+        [Test]
+        public void DriveHasLetter()
+        {
+            Drive f = new Drive('F');
+            Assert.That(f.DriveLetter == 'F');
+        }
+    }
+}
